51dda23379f534215d79e2ea046e2f7cab9317b7
John Collins <johnscollins98@gmail.com>
2021-03-20 16:20:37 +0000

    Add GetNeedsPromotion tests

---
+++ tests/DataProcessorTests/GetNeedsPromotion.test.js
@@ -0,0 +1,37 @@
+const { DataProcessor } = require('../../utils/DataProcessor');
+const {
+  mockValidRanks,
+  mockDiscordMembers,
+  mockGW2Members,
+} = require('../mocks/mockData');
+
+test('GetNeedsPromotion without failing', () => {
+  const dataProcessor = new DataProcessor(
+    mockGW2Members,
+    mockDiscordMembers,
+    mockValidRanks
+  );
+  dataProcessor.getNeedsPromotion();
+});
+
+test('GetNeedsPromotion gets correct object', () => {
+  const dataProcessor = new DataProcessor(
+    mockGW2Members,
+    mockDiscordMembers,
+    mockValidRanks
+  );
+  expect(dataProcessor.getNeedsPromotion()[0].name).toEqual("NeedsPromoted.5432");
+});
+
+test('GetNeedsPromotion with no candidates', () => {
+  const idx = mockGW2Members.findIndex(o => o.name === "NeedsPromoted.5432");
+  const ourGw2Members = [...mockGW2Members];
+  ourGw2Members.splice(idx);
+  
+  const dataProcessor = new DataProcessor(
+    ourGw2Members,
+    mockDiscordMembers,
+    mockValidRanks
+  )
+  expect(dataProcessor.getNeedsPromotion()).toEqual([]);
+})
