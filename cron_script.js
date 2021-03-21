7efb3fa3997814f4addfcc5a7e6be58a7453034a
John Collins <johnscollins98@gmail.com>
2021-03-21 02:04:33 +0000

    Set up for cronjob to send requiredActions

---
+++ cron_script.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Client } = require('discord.js');
+const { commands } = require('./utils/Commands');
+const client = new Client();
+
+client.on('ready', async () => {
+  try {
+    console.log(`Logged in as ${client.user.tag}`)
+    const guild = client.guilds.resolve(process.env.DISCORD_GUILD_ID);
+    if (!guild) throw "Can't find guild";
+    
+    const channel = guild.channels.resolve(process.env.DISCORD_CHANNEL_ID);
+    if (!channel) throw "Can't find channel";
+
+    await commands({
+      content: "--requiredActions",
+      author: { id: process.env.TO_TAG },
+      channel,
+      guild
+    })
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.destroy();
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
