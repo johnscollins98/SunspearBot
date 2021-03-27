45251748f80fd1fb3b3f5e46991510e0fec7903a
John Collins <johnscollins98@gmail.com>
2021-03-27 18:25:59 +0000

    Add support for "prefix" command in MessageHandler - user can set the prefix - user can get the prefix

--- src/utils/MessageHandler.js
+++ src/utils/MessageHandler.js
@@ -22,12 +22,11 @@ class MessageHandler {
     if (msg.author.bot) return;
 
     const prefix = (await this.guildConfigRepo.getPrefix()) || '^';
-    const match = await this._hasPrefix(msg, prefix);
+    const match = await this.parseCommand(msg, prefix);
     if (match) {
-      const usedPrefix = match[1];
-      const strippedMsg = match[2];
-      if (!strippedMsg && usedPrefix !== prefix) {
-        msg.reply(`The prefix for this server is \`${prefix}\`.`);
+      const [_, usedPrefix, command, args] = match;
+      if (command === 'prefix' || (!command && usedPrefix !== prefix)) {
+        this.prefixCommand(msg, args, prefix);
       } else {
         msg.reply('Could not find this command.');
       }
@@ -38,14 +37,33 @@ class MessageHandler {
    * Check a Message has the expected prefix
    *
    * @param {Message} msg message to check
-   * @returns {boolean} true if it has a prefix we expect
+   * @returns {RegExpMatchArray} true if it has a prefix we expect
    */
-  async _hasPrefix(msg, prefix) {
+  async parseCommand(msg, prefix) {
     const content = msg.content;
+    const regex = `^(\\${prefix}|<@!?${this.client.user.id}>\\s?)([a-zA-Z]*)?\\s?(.*)?$`;
+    return content.match(new RegExp(regex, 'i'));
+  }
 
-    return content.match(
-      new RegExp(`(\\${prefix}|<@\\!?${this.client.user.id}>\\s?)(\.*)`)
-    );
+  /**
+   * Command for setting prefix
+   *
+   * @param {Message} msg message that initiated command
+   * @param {string} newPrefix value to set the prefix as
+   * @param {string} oldPrefix current value of the prefix
+   */
+  async prefixCommand(msg, newPrefix, oldPrefix) {
+    if (!newPrefix) {
+      return await msg.reply(`The prefix for this server is \`${oldPrefix}\`.`);
+    }
+
+    const match = newPrefix.trim().match(/^[-!"£$%^&*_=+]+$/);
+    if (!match) {
+      return await msg.reply(`Usage: \`${oldPrefix}prefix [-!"£$%^&*_=+]\``);
+    } else {
+      const response = await this.guildConfigRepo.setPrefix(newPrefix.trim());
+      return await msg.reply(`Set the prefix to \`${response.prefix}\`.`);
+    }
   }
 }
 
--- tests/MessageHandlerTests/MessageHandler.test.js
+++ tests/MessageHandlerTests/MessageHandler.test.js
@@ -3,8 +3,20 @@ const GuildConfigRepository = require('../../src/repositories/GuildConfigReposit
 const { Message, Client } = require('discord.js');
 
 jest.mock('../../src/repositories/GuildConfigRepository');
-jest.mock('../../src/utils/DataProcessor');
+GuildConfigRepository.mockImplementation(() => ({
+  getPrefix: jest.fn().mockReturnValue('+'),
+  setPrefix: jest.fn().mockImplementation((prefix) => ({ prefix })),
+}));
+
 jest.mock('discord.js');
+Message.mockImplementation(() => ({
+  reply: jest.fn(),
+  author: {},
+}));
+
+Client.mockImplementation(() => ({
+  user: { id: 123 },
+}));
 
 describe('handleMessage', () => {
   /** @type {MessageHandler} */
@@ -22,15 +34,11 @@ describe('handleMessage', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     guildConfigRepo = new GuildConfigRepository();
-    guildConfigRepo.getPrefix.mockReturnValue('+');
-
     message = new Message();
-    message.author = {};
-
     client = new Client();
-    client.user = { id: 123 };
 
     messageHandler = new MessageHandler(guildConfigRepo, client);
+    messageHandler.prefixCommand = jest.fn();
   });
 
   it('should run without failing', async () => {
@@ -90,28 +98,34 @@ describe('handleMessage', () => {
     expect(message.reply).toHaveBeenCalledWith('Could not find this command.');
   });
 
-  it('should reply with the prefix if message is only a mention', async () => {
+  it('should call prefixCommand if message is only a mention', async () => {
     message.content = `<@${client.user.id}>`; // no space/!
     await messageHandler.handle(message);
 
-    expect(message.reply).toHaveBeenCalledWith(
-      'The prefix for this server is `+`.'
+    expect(messageHandler.prefixCommand).toHaveBeenCalledWith(
+      message,
+      undefined,
+      '+'
     );
 
     message.reply.mockClear();
 
     message.content = `<@!${client.user.id}>`; // with !
     await messageHandler.handle(message);
-    expect(message.reply).toHaveBeenLastCalledWith(
-      'The prefix for this server is `+`.'
+    expect(messageHandler.prefixCommand).toHaveBeenCalledWith(
+      message,
+      undefined,
+      '+'
     );
 
     message.reply.mockClear();
 
     message.content = `<@${client.user.id}> `; // with a space
     await messageHandler.handle(message);
-    expect(message.reply).toHaveBeenLastCalledWith(
-      'The prefix for this server is `+`.'
+    expect(messageHandler.prefixCommand).toHaveBeenCalledWith(
+      message,
+      undefined,
+      '+'
     );
   });
 
@@ -120,5 +134,84 @@ describe('handleMessage', () => {
     await messageHandler.handle(message);
 
     expect(message.reply).toHaveBeenCalledWith('Could not find this command.');
+  });
+
+  it('should call prefixCommand for "prefix" command', async () => {
+    message.content = '+prefix !';
+    messageHandler.prefixCommand = jest.fn();
+    await messageHandler.handle(message);
+
+    expect(messageHandler.prefixCommand).toHaveBeenCalled();
+    expect(messageHandler.prefixCommand).toHaveBeenCalledWith(
+      message,
+      '!',
+      '+'
+    );
+  });
+});
+
+describe('prefixCommand', () => {
+  /** @type {MessageHandler} */
+  let messageHandler;
+
+  /** @type {GuildConfigRepository} */
+  let guildConfigRepo;
+
+  /** @type {Message} */
+  let message;
+
+  /** @type {Client} */
+  let client;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    guildConfigRepo = new GuildConfigRepository();
+    message = new Message();
+    client = new Client();
+
+    messageHandler = new MessageHandler(guildConfigRepo, client);
+  });
+
+  it('should reply with the prefix if args is undefined', async () => {
+    await messageHandler.prefixCommand(message, undefined, '+');
+    expect(message.reply).toHaveBeenCalledWith(
+      'The prefix for this server is `+`.'
+    );
+  });
+
+  it('should complain if called with unsupported prefx', async () => {
+    await messageHandler.prefixCommand(message, 'asdf!asdf', '+');
+    expect(message.reply).toHaveBeenCalledWith(
+      'Usage: `+prefix [-!"£$%^&*_=+]`'
+    );
+  });
+
+  it('should save the prefix if it is supported', async () => {
+    const testCases = [
+      '-',
+      '!',
+      '!!!', // multiple
+      '! ', // space after
+      ' !', // space before
+      '"',
+      '£',
+      '$',
+      '%',
+      '^',
+      '&',
+      '*',
+      '_',
+      '=',
+      '+',
+    ];
+
+    for (const testCase of testCases) {
+      const res = await messageHandler.prefixCommand(message, testCase, '+');
+      
+      expect(guildConfigRepo.setPrefix).toHaveBeenCalledWith(testCase.trim());
+      expect(message.reply).toHaveBeenCalledWith(`Set the prefix to \`${testCase.trim()}\`.`);
+
+      guildConfigRepo.setPrefix.mockClear();
+    }
   });
 });
